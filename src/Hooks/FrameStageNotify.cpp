bda5a1794e9c3e4b9bc08278b5d83ee9442183e9
TheDankPaster <TheDankPaster@users.noreply.github.com>
2017-07-13 14:52:15 -0400

    Update FrameStageNotify.cpp

--- src/Hooks/FrameStageNotify.cpp
+++ src/Hooks/FrameStageNotify.cpp
@@ -11,8 +11,7 @@ void Hooks::FrameStageNotify(void* thisptr, ClientFrameStage_t stage)
 	SkyBox::FrameStageNotify(stage);
 	ASUSWalls::FrameStageNotify(stage);
 	NoSmoke::FrameStageNotify(stage);
-	ThirdPerson::FrameStageNotify(stage);
-
+	
 	if (SkinChanger::forceFullUpdate)
 	{
 		GetLocalClient(-1)->m_nDeltaTick = -1;
